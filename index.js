63fc316bf0fd199a4e7e2d446f1a0e544dc61506
sonu kumar <sonu36437@gmail.com>
2025-03-30 13:23:16 +0530

    added headers

--- index.js
+++ index.js
@@ -7,6 +7,29 @@ app.get('/',(req,res)=>{
     res.status(200).json("health is fine");
 })
 
+const fetchJioSaavnResults = async (queryParam) => {
+    try {
+        const response = await axios.get(
+            `https://www.jiosaavn.com/api.php?__call=search.getResults&_format=json&_marker=0&api_version=4&ctx=web6dot0&q=${queryParam}&p=0&n=1`,
+            {
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+                    'Accept-Language': 'en-US,en;q=0.9',
+                    'Connection': 'keep-alive',
+                    'cache-control':'max-age=0'
+                }
+            }
+        );
+   
+        
+        return response;
+
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+};
+
 app.get("/search", async (req, res) => {
     try {
         const queryParam = req.query.q;
@@ -14,7 +37,7 @@ app.get("/search", async (req, res) => {
             return res.status(400).json({ error: "Missing query parameter 'q'" });
         }
 
-        const response = await axios.get(`https://www.jiosaavn.com/api.php?__call=search.getResults&_format=json&_marker=0&api_version=4&ctx=web6dot0&q=${queryParam}&p=0&n=1`);
+      const response= await fetchJioSaavnResults(queryParam);
         
         const results = response.data?.results;
         if (!results || results.length === 0) {
